73f9e0185885c8cce33975e0538f708fc3518111
Paweł Kornas <kornasnorge@gmail.com>
2022-06-27 15:29:20 +0200

    User information

--- client/src/components/ProviderDetails.js
+++ client/src/components/ProviderDetails.js
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
+import { FaEdit } from "react-icons/fa";
 function ProviderDetails({ user }) {
   console.log(user);
-  return (
-    <div className="user-info">
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
+  const [old, setOld] = useState(user);
+  const [updated, setUpdated] = useState("");
 
-      <p></p>
-    </div>
+  const updateInfo = (e) => {
+    console.log(e.target);
+  };
+  return (
+    <>
+      <h2 className="to-edit">User information</h2>
+      <button className="edit" onClick={updateInfo}>
+        <FaEdit />
+      </button>
+      <div className="user-info">
+        <div>
+          <span>name: {user.name}</span>
+        </div>
+        <div>
+          <span>e-mail: {user.eMail}</span>
+        </div>
+        <div>
+          <span>Company name: {user.companyName}</span>
+        </div>
+      </div>
+    </>
   );
 }
 export default ProviderDetails;
--- client/src/containers/UserDashBoard.js
+++ client/src/containers/UserDashBoard.js
@@ -1,13 +1,17 @@
 import AddProduct from "./AddProduct";
 import UserProducts from "./UserProducts";
 import ProviderDetails from "../components/ProviderDetails";
+
 import { useState, useEffect } from "react";
 
 function UserDashBoard({ user }) {
   return (
     <>
+      <h2>Add product</h2>
       <AddProduct user={user} />
+
       <ProviderDetails user={user} />
+      <h2>Your products</h2>
       <UserProducts user={user} />
     </>
   );
