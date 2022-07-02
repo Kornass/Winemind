93058af2d482715ff76c01d906db8dfedc600210
Alex Castro <alex.castro.ver@gmail.com>
2022-07-02 17:17:58 +0200

    Searchbar 100% functional

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -14,29 +14,34 @@ function Search({ allProd, onAdd }) {
   };
 
   const renderSearch = () => {
-    // searchresults - allProd.filter
-
-    return allProd.map((ele, idx) => {
-      return ele.name.toUpperCase().includes(list.toUpperCase()) ||
+    const filteredList = allProd.filter((ele) => {
+      return (
+        ele.name.toUpperCase().includes(list.toUpperCase()) ||
         ele.type.toUpperCase().includes(list.toUpperCase()) ||
         ele.grape.toUpperCase().includes(list.toUpperCase()) ||
         ele.country.toUpperCase().includes(list.toUpperCase()) ||
         ele.producer.toUpperCase().includes(list.toUpperCase()) ||
-        ele.region.toUpperCase().includes(list.toUpperCase()) ? (
-        <div key={idx}>
-          <Link to={`/single/${ele.sku}`} key={ele._id}>
-            <div className="searchStuff">
-              <img className="searchImg" src={ele.img} />
-              <p className="searchTitle">{ele.name}</p>
-              <p className="searchTitle">{ele.price}€</p>
-            </div>
-          </Link>
-          <button onClick={() => onAdd(ele)}>Add to cart</button>
-        </div>
-      ) : (
-        <h2>No products match</h2>
+        ele.region.toUpperCase().includes(list.toUpperCase())
       );
     });
+    if (filteredList.length > 0) {
+      return filteredList.map((ele, idx) => {
+        return (
+          <div key={idx}>
+            <Link to={`/single/${ele.sku}`} key={ele._id}>
+              <div className="searchStuff">
+                <img className="searchImg" src={ele.img} />
+                <p className="searchTitle">{ele.name}</p>
+                <p className="searchTitle">{ele.price}€</p>
+              </div>
+            </Link>
+            <button onClick={() => onAdd(ele)}>Add to cart</button>
+          </div>
+        );
+      });
+    } else {
+      return <h2>No products match this search</h2>;
+    }
   };
   return (
     <>
