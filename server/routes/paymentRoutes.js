6e77d3c99847ce51271a673e5117469b2f19935c
Paweł Kornas <kornasnorge@gmail.com>
2022-07-06 12:33:50 +0200

    Deployment maitenance

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const controller = require("../controllers/paymentcontroller.js");
+const controller = require("../controllers/paymentController.js");
 
 router.post("/create-checkout-session", controller.create_checkout_session);
 router.get("/checkout-session", controller.checkout_session);
